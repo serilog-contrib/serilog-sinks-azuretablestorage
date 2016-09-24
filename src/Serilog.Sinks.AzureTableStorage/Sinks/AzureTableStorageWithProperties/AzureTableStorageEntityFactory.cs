50651b3c395d6801d92b8b415602554f86db64ba
Chris Simmons <chris.dev@netchris.com>
2016-09-24 12:15:37 -0400

    Added carriage return and linefeed characters to disallowed row key chars
    Fixes #10

--- src/Serilog.Sinks.AzureTableStorage/Sinks/AzureTableStorageWithProperties/AzureTableStorageEntityFactory.cs
+++ src/Serilog.Sinks.AzureTableStorage/Sinks/AzureTableStorageWithProperties/AzureTableStorageEntityFactory.cs
@@ -28,7 +28,7 @@ namespace Serilog.Sinks.AzureTableStorage
 	public static class AzureTableStorageEntityFactory
 	{
 		// Valid RowKey name characters
-		static readonly Regex _rowKeyNotAllowedMatch = new Regex(@"(\\|/|#|\?)");
+		static readonly Regex _rowKeyNotAllowedMatch = new Regex(@"(\\|/|#|\n|\r|\?)");
 
 		// Azure tables support a maximum of 255 properties. PartitionKey, RowKey and Timestamp
 		// bring the maximum to 252.
